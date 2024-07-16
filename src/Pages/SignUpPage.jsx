cfb53f1c88810c059b91fae62b190a7585eb2ef9
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-07-17 00:29:59 +0530

    changed

--- src/Pages/SignUpPage.jsx
+++ src/Pages/SignUpPage.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './SignUpPage.css';
 
 function SignUpPage() {
   return (
     <div className='signup-container'>
         <form className='signup-form'>
             <h1>Sign Up</h1>
+            <input type='email' placeholder='Email' />
+            <input type='password' placeholder='Password' />
+            <button type='submit'>Sign Up</button>
+            <p>Already have an account? <a href='/'>Sign In</a></p>
         </form>
     </div>
   )
