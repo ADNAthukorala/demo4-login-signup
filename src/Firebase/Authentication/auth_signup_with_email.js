5e4045b451d33d1dd337710280ac9874e76904fc
Dumindu Athukorala <117508037+ADNAthukorala@users.noreply.github.com>
2024-07-16 23:16:22 +0530

    changed

---
+++ src/Firebase/Authentication/auth_signup_with_email.js
@@ -0,0 +1,15 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+export const signUpWithEmailAndPassword = async(auth, email, password) => {
+  await createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    alert('You have successfully signed up!');
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert('An error has occurred!');
+  });
+}
